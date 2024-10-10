f7e4098cdf8bfee1db0240f4be8cf11f6bab9ce0
hanra15 <farhanrajab15@gmail.com>
2024-10-10 10:13:45 +0700

    app done

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,31 +3,47 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Transaction;
+use Carbon\Carbon;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
+
     protected static ?string $pollingInterval = '10s';
     protected static bool $isLazy = true;
     protected function getStats(): array
     {
-        $pemasukan = Transaction::incomes()->get()->sum('amount');
-        $pengeluaran = Transaction::expenses()->get()->sum('amount');
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
+        $pemasukan = Transaction::incomes()
+            ->whereBetween('transaction_date', [$startDate, $endDate])
+            ->sum('amount');
+        $pengeluaran = Transaction::expenses()
+            ->whereBetween('transaction_date', [$startDate, $endDate])
+            ->sum('amount');
         $selisih = $pemasukan - $pengeluaran;
         // $pemasukan = Transaction::join('categories', 'transactions.category_id', '=', 'categories.id')->where('is_expense', false)->sum('amount');
         // $pengeluaran = Transaction::join('categories', 'transactions.category_id', '=', 'categories.id')->where('is_expense', true)->sum('amount');
         return [
             //
-            Stat::make('Total Pemasukan', $pemasukan)
+            Stat::make('Total Pemasukan', 'Rp.' . ' ' . $pemasukan)
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->description('increase')
                 ->color('success'),
-            Stat::make('Total Pengeluaran', $pengeluaran)
+            Stat::make('Total Pengeluaran', 'Rp.' . ' ' . $pengeluaran)
                 ->description('decrease')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->color('danger'),
-            Stat::make('Selisih', $selisih)
+            Stat::make('Selisih', 'Rp.' . ' ' . $selisih)
                 ->description('money on pocket')
                 ->descriptionIcon('heroicon-s-currency-dollar')
                 ->color('info'),
--- app/Filament/Widgets/WIncomeChart.php
+++ app/Filament/Widgets/WIncomeChart.php
@@ -3,24 +3,36 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Transaction;
+use Carbon\Carbon;
 use Filament\Widgets\ChartWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
 
 class WIncomeChart extends ChartWidget
 {
+    use InteractsWithPageFilters;
+
     protected static ?string $heading = 'Pemasukan';
     protected static string $color = 'success';
 
     protected function getData(): array
     {
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
         $data = Trend::query(Transaction::incomes())
             ->between(
-                start: now()->startOfYear(),
-                end: now()->endOfYear(),
+                start: $startDate,
+                end: $endDate,
             )
-            ->perMonth()
-            ->count();
+            ->perDay()
+            ->sum('amount');
 
         return [
             'datasets' => [
